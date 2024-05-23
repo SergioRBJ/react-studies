72be48d96a23ebd6ae79c16ed041c694fe8db327
SergioRBJ <sergiorbj93@gmail.com>
2024-05-23 10:01:50 -0300

    feat(pizza-menu): composition of pizza menu

--- pizza-menu/src/App.jsx
+++ pizza-menu/src/App.jsx
@@ -3,10 +3,32 @@ import "./App.css";
 function App() {
   return (
     <>
+      <Header />
+      <Menu />
+      <Footer />
+    </>
+  );
+}
+
+function Header() {
+  return <h1>Fast React Pizza Co.</h1>;
+}
+
+function Menu() {
+  return (
+    <>
+      <h2>Our menu</h2>;
       <Pizza />
     </>
   );
 }
+
+function Footer() {
+  return (
+    <footer> {new Date().toLocaleDateString()} We're currently open!</footer>
+  );
+}
+
 function Pizza() {
   return (
     <>
