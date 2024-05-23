b05d7026c5bde2e3b0f6a4b639ab46643b0242f0
SergioRBJ <sergiorbj93@gmail.com>
2024-05-23 10:07:55 -0300

    feat(pizza-menu): alert for open/close

--- pizza-menu/src/App.jsx
+++ pizza-menu/src/App.jsx
@@ -17,17 +17,27 @@ function Header() {
 function Menu() {
   return (
     <>
-      <h2>Our menu</h2>;
+      <h2>Our menu</h2>
       <Pizza />
     </>
   );
 }
 
 function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+
+  if (hour >= openHour && hour <= closeHour) alert("We're currently open!");
+  else alert("We're close!");
+
   return (
-    <footer> {new Date().toLocaleDateString()} We're currently open!</footer>
+    <footer>
+      {new Date().toLocaleDateString()} {"We're currently open!"}
+    </footer>
   );
 }
+``;
 
 function Pizza() {
   return (
