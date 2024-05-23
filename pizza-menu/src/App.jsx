701c981854b1cde4fcd0dd8cd065500c7914be1c
SergioRBJ <sergiorbj93@gmail.com>
2024-05-22 23:03:54 -0300

    feat(pizza-menu): add title and image

---
+++ pizza-menu/src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Pizza />
+    </>
+  );
+}
+function Pizza() {
+  return (
+    <>
+      <h1>Pizza</h1>
+      <img src="pizzas/spinaci.jpg" alt="Spinaci pizza" />
+      <div>Tomato, mozarella, spinach, and ricotta cheese</div>
+    </>
+  );
+}
+
+export default App;
